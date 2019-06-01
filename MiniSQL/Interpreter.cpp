b210a0d6d4866df93d7a7b98564e903485ad8ff3
aishenji <3170102491@zju.edu.cn>
2019-06-01 14:54:08 +0800

    add Interpreter

---
+++ MiniSQL/Interpreter.cpp
@@ -0,0 +1,9 @@
+//
+//  Interpreter.cpp
+//  MiniSQL
+//
+//  Created by 沈霁 on 2019/6/1.
+//  Copyright © 2019 Zhao Weikai, Shen Ji, Chen Huan. All rights reserved.
+//
+
+#include "Interpreter.hpp"
