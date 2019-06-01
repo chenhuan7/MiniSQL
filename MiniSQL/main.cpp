4ab95df8296b0dda95f58af17ee76c1cb8d6a941
chenhuan7 <690174861@qq.com>
2019-06-01 14:15:06 +0800

    first

---
+++ MiniSQL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniSQL
+//
+//  Created by Domoto Yasu on 2019/6/1.
+//  Copyright © 2019 Zhao Weikai, Shen Ji, Chen Huan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
