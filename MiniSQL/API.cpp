385c0435d34423b0b647bf1cd3bfb1a9dda183e2
aishenji <3170102491@zju.edu.cn>
2019-06-03 15:19:05 +0800

    add some API

--- MiniSQL/API.cpp
+++ MiniSQL/API.cpp
@@ -12,3 +12,32 @@ using namespace std;
 void API::createTable(const std::string &tableName, const std::vector<AttributeType> &data, int primaryKey) {
     
 }
+void API::dropTable(const std::string &tableName) {
+//    if (API::catalogManager.haveTable(tableName))
+    
+    API::catalogManager.dropTable(tableName);
+}
+
+void API::createIndex(const std::string &tableName, const std::string &indexName, const std::string &attributeName) {
+//    if (API::catalogManager.haveTable(tableName))
+    
+    
+    int id = -1;
+    API::catalogManager.createIndex(tableName, indexName, id);
+}
+
+void API::dropIndex(const std::string &indexName) {
+    
+}
+
+void API::selectRecord(const std::string &tableName, const std::vector<std::string> &condition){
+    
+}
+
+void API::insertRecord(const std::string &tableName, const std::vector<std::string> &value){
+    
+}
+
+void API::deleteRecord(const std::string &tableName, const std::vector<std::string> &condition){
+    
+}
