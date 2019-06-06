adaf67bedbef80bd3a135067aada0a308049d510
aishenji <3170102491@zju.edu.cn>
2019-06-06 18:16:03 +0800

    update

--- MiniSQL/API.cpp
+++ MiniSQL/API.cpp
@@ -10,17 +10,25 @@
 using namespace std;
 
 void API::createTable(const std::string &tableName, const std::vector<AttributeType> &data, int primaryKey) {
+    if (API::catalogManager.hasTable(tableName)) {
+        std::cout << "table already exits" << std::endl;
+        return;
+    }
     
+    Table newTable = API::catalogManager.createTable(tableName, data);
 }
 void API::dropTable(const std::string &tableName) {
-//    if (API::catalogManager.haveTable(tableName))
+    if (!API::catalogManager.hasTable(tableName)){
+        std::cout << "table does not exit" << std::endl;
+        return;
+    }
     
     API::catalogManager.dropTable(tableName);
 }
 
 void API::createIndex(const std::string &tableName, const std::string &indexName, const std::string &attributeName) {
-//    if (API::catalogManager.haveTable(tableName))
-    
+//    if (API::catalogManager.hasTable(tableName))
+//        std::cout << "" << std::endl;
     
     int id = -1;
     API::catalogManager.createIndex(tableName, indexName, id);
