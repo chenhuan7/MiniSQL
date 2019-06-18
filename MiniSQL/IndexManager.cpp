3df8122456f83571e3a54395f02d66d2a5115ba0
aishenji <3170102491@zju.edu.cn>
2019-06-18 21:54:21 +0800

    index

--- MiniSQL/IndexManager.cpp
+++ MiniSQL/IndexManager.cpp
@@ -6,9 +6,9 @@
 //  Copyright © 2019 Zhao Weikai, Shen Ji, Chen Huan. All rights reserved.
 //
 #include "IndexManager.hpp"
-using namespace std;
-bool isExist(const string &fileName) {
-    ifstream f(fileName.c_str());
+
+bool isExist(const std::string &fileName) {
+    std::ifstream f(fileName.c_str());
     if (f.good()) {
         f.close();
         return true;
@@ -17,9 +17,52 @@ bool isExist(const string &fileName) {
         return false;
     }
 }
-void IndexManager::createIndex(const std::string &indexName, const std::string &dbName, Table &table, const AttributeType &attr){
-    
+
+void IndexManager::createIndex(const std::string &indexName, int type){
+    if (type == TYPE_INT) {
+        BPlusTree<int> *tree = new BPlusTree<int>(indexName, sizeof(int), 11);
+        indexIntMap.insert(intMap::value_type(indexName, tree));
+        return ;
+    }
+    if (type == TYPE_FLOAT) {
+        BPlusTree<float> *tree = new BPlusTree<float>(indexName, sizeof(float), 13);
+        indexFloatMap.insert(floatMap::value_type(indexName, tree));
+        return ;
+    }
+    if (type > 0) {
+        BPlusTree<std::string> *tree = new BPlusTree<std::string>(indexName, sizeof(char)*type, 15);
+        indexStringMap.insert(stringMap::value_type(indexName, tree));
+        return ;
+    }
+    return;
 }
+
 void IndexManager::dropIndex(const std::string &indexName){
-    
+    intMap::iterator itInt = indexIntMap.find(indexName);
+    if (itInt != indexIntMap.end()) {
+        delete itInt->second;
+        indexIntMap.erase(itInt);
+        return;
+    }
+    floatMap::iterator itFloat = indexFloatMap.find(indexName);
+    if (itFloat != indexFloatMap.end()) {
+        delete itFloat->second;
+        indexFloatMap.erase(itFloat);
+        return;
+    }
+    stringMap::iterator itString = indexStringMap.find(indexName);
+    if (itString != indexStringMap.end()) {
+        delete itString->second;
+        indexStringMap.erase(itString);
+        return;
+    }
+    std::cout << "Error: no such index" << std::endl;
+    return;
+}
+
+void IndexManager::insertIndex(const std::string &indexName, const element &e, long offset) {
+    if(e.type == TYPE_INT) {
+        
+    }
+    return;
 }
--- MiniSQL/IndexManager.hpp
+++ MiniSQL/IndexManager.hpp
@@ -10,11 +10,27 @@
 #define IndexManager_hpp
 
 #include "MiniSQL.h"
+#include "BPlusTree.hpp"
 
 class IndexManager {
-    void createIndex(const std::string &indexName, const std::string &dbName, Table &table, const AttributeType &attr);
+public:
+    void createIndex(const std::string &indexName, int type);
     void dropIndex(const std::string &indexName);
     void insertIndex(const std::string &indexName, const element &e, long offset);
+    int findIndex(const std::string &indexName);
+    void deleteIndex(const std::string &indexName, const element &e);
+    void searchRange(const std::string &indexName, element data1, element data2, std::vector<int>& values);
+    
+private:
+    int static const TYPE_FLOAT = 0;
+    int static const TYPE_INT = -1;
+    
+    typedef std::map<std::string, BPlusTree<int> *> intMap;
+    typedef std::map<std::string, BPlusTree<float> *> floatMap;
+    typedef std::map<std::string, BPlusTree<std::string> *> stringMap;
+    std::map<std::string, BPlusTree<int> *> indexIntMap;
+    std::map<std::string, BPlusTree<float> *> indexFloatMap;
+    std::map<std::string, BPlusTree<std::string> *> indexStringMap;
     
 };
 
